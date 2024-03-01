6610f35c0d8a6d9f0b5a952c07e79ef931eae033
Vino-Rishi <vinothbala2073@gmail.com>
2024-03-01 18:35:17 +0530

    Modal-Created

---
+++ index.js
@@ -0,0 +1,9 @@
+const state = {
+  taskList: [],
+};
+
+// DOM Operations
+const taskContents = document.querySelector(".task__contents");
+const taskModal = document.querySelector(".task__modal__body");
+
+const htmlTaskContent = ({ id, title, description, type, url }) => {};
