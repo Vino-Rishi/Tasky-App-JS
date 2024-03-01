d463fee20bf7c4078b19deda854f872106828979
Vino-Rishi <vinothbala2073@gmail.com>
2024-03-01 19:49:46 +0530

    Fix

---
+++ index.js
@@ -0,0 +1,15 @@
+const state = {
+  taskList: [],
+};
+
+// DOM Operations
+const taskContents = document.querySelector(".task__contents");
+const taskModal = document.querySelector(".task__modal__body");
+
+const htmlTaskContent = ({ id, title, description, type, url }) => {
+<div class="col-sm-6 col-md-4 col-lg-3">
+  <div class="card_task">
+
+  </div>
+</div>
+};
